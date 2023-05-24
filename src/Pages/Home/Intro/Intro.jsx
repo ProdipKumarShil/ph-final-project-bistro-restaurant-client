7fe574730bec3906daf0afd70856b19e2ef9d616
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-24 11:54:38 +0600

    restore home components

--- src/Pages/Home/Intro/Intro.jsx
+++ src/Pages/Home/Intro/Intro.jsx
@@ -1,8 +1,16 @@
+import img from '../../../assets/home/chef-service.jpg'
 
 const Intro = () => {
   return (
-    <div>
-      this is intro
+    <div className=''>
+      <img className='h-[572px] object-cover ' src={img} alt="" />
+      <div className="relative bg-white">
+        <h1>Bistro Boss</h1>
+        <p>
+          Welcome to Bistro Boss, where culinary excellence meets warm hospitality! Nestled in the heart of City, our restaurant is a haven for food enthusiasts seeking an unforgettable dining experience. With our passion for flavor, creativity, and impeccable service, we aim to surpass your expectations and leave you craving for more.
+          As you step into Bistro Boss, youll be embraced by an inviting and charming ambiance. The soothing lighting, tasteful decor, and cozy seating arrangement create an atmosphere that exudes both elegance and comfort. Whether youre looking to enjoy a romantic evening, celebrate a special occasion, or simply savor a delicious meal, our restaurant provides the perfect backdrop for any dining affair.
+        </p>
+      </div>
     </div>
   );
 };
---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,11 @@
+
+const SectionTitle = ({heading, subHeading}) => {
+  return (
+    <div className="mx-auto text-center w-1/2 md:w-4/12 my-8">
+      <p className="text-yellow-600 mb-2 italic text-xl font-[400]">--- {subHeading} ---</p>
+      <h3 className=" text-4xl border-y-4 py-4 uppercase">{heading}</h3>
+    </div>
+  );
+};
+
+export default SectionTitle;
