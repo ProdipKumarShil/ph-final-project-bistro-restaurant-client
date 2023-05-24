4fa10d346884d6b656a2cf3d0d8ee25b0f95c989
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-24 10:44:18 +0600

    fix swiper

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -20,22 +20,27 @@ const Category = () => {
         clickable: true,
       }}
       modules={[Pagination]}
-      className="mySwiper"
+      className="mySwiper mb-24"
     >
-      <SwiperSlide>
+      <SwiperSlide >
         <img src={slide1} alt="" />
+        <h3 className="text-white text-center -mt-16 text-4xl uppercase">Salads</h3>
       </SwiperSlide>
       <SwiperSlide>
         <img src={slide2} alt="" />
+        <h3 className="text-white text-center -mt-16 text-4xl uppercase">Pizzas</h3>
       </SwiperSlide>
       <SwiperSlide>
         <img src={slide3} alt="" />
+        <h3 className="text-white text-center -mt-16 text-4xl uppercase">Soups</h3>
       </SwiperSlide>
       <SwiperSlide>
         <img src={slide4} alt="" />
+        <h3 className="text-white text-center -mt-16 text-4xl uppercase">Desserts</h3>
       </SwiperSlide>
       <SwiperSlide>
         <img src={slide5} alt="" />
+        <h3 className="text-white text-center -mt-16 text-4xl uppercase">Salads</h3>
       </SwiperSlide>
       
     </Swiper>
