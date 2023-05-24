68c4af8bea0b38643d8db56bdb515203ee0928e7
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-24 17:04:23 +0600

    added rating

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -4,9 +4,9 @@ import './feature.css'
 
 const Featured = () => {
   return (
-    <div className="featured-items text-white my-20">
-      <SectionTitle subHeading='check it out' heading='Featured Item'></SectionTitle>
-      <div className="md:flex justify-center py-20 items-center pt-12 px-36">
+    <div className="featured-items bg-fixed  text-white my-20">
+      <SectionTitle classProp={'bg-slate-500 w-full bg-opacity-60 '} subHeading='check it out' heading='Featured Item'></SectionTitle>
+      <div className="md:flex justify-center bg-slate-500 bg-opacity-60 py-20 items-center pt-12 px-36">
         <div className="">
           <img src={featuredImg} alt="" />
         </div>
@@ -14,7 +14,7 @@ const Featured = () => {
           <p>Aug 20, 2029</p>
           <p className="uppercase">where can i get some?</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque earum omnis esse laborum beatae dolore repudiandae recusandae perferendis provident eius?</p>
-          <button className="btn btn-outline">Order Now!!</button>
+          <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now!!</button>
         </div>
       </div>
     </div>
--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,7 +1,7 @@
 
 const SectionTitle = ({heading, subHeading}) => {
   return (
-    <div className="mx-auto text-center w-1/2 md:w-4/12 my-8">
+    <div className="mx-auto text-center w-1/2 md:w-4/12 py-8">
       <p className="text-yellow-600 mb-2 italic text-xl font-[400]">--- {subHeading} ---</p>
       <h3 className=" text-4xl border-y-4 py-4 uppercase">{heading}</h3>
     </div>
