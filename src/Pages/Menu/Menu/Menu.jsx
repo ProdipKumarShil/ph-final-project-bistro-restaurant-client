1d4e28b1ef79aedeb23a2c770e7c797dfa267fed
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-25 09:28:04 +0600

    added offered menu items

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,16 +1,29 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 
 const Menu = () => {
-  
+  const [menu] = useMenu()
+  const dessert = menu.filter(item => item.category === 'dessert')
+  const soup = menu.filter(item => item.category === 'soup')
+  const salad = menu.filter(item => item.category === 'salad')
+  const pizza = menu.filter(item => item.category === 'pizza')
+  const offered = menu.filter(item => item.category === 'offered')
+
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
       <Cover img={menuImg} title="our menu"></Cover>
-      
+      {/* main cover */}
+      <SectionTitle subHeading={"Don't Miss"} heading={"Today's Offer"}></SectionTitle>
+      {/* offered menu items */}
+      <MenuCategory items={offered}></MenuCategory>
+      {/*  */}
     </div>
   );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,19 @@
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 
-const MenuCategory = () => {
+const MenuCategory = ({items, title, coverImg}) => {
   return (
     <div>
-      
+      {title && <Cover img={coverImg} title="our menu"></Cover>}
+      <div className="grid md:grid-cols-2 gap-10 mb-12">
+        {
+          items.map(item => <MenuItem
+            key={item._id}
+            item={item}
+          ></MenuItem>)
+        }
+      </div>
     </div>
   );
 };
