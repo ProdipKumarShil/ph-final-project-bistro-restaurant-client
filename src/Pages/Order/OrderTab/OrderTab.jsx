888133c3ed4252b8a6618f5d90ac461aa918f025
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-26 07:23:33 +0600

    get login page without header and footer

--- src/Pages/Order/OrderTab/OrderTab.jsx
+++ src/Pages/Order/OrderTab/OrderTab.jsx
@@ -1,15 +1,64 @@
 import FoodCard from '../../../components/FoodCard/FoodCard';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
-const OrderTab = ({items}) => {
+const OrderTab = ({ items }) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
   return (
-    <div className="grid md:grid-cols-3 gap-10">
-      {
-        items.map(item => <FoodCard
-          key={item._id}
-          item={item}
-        ></FoodCard>)
-      }
-    </div>
+    // <div className="grid md:grid-cols-3 gap-10">
+    //   {
+    //     items.map(item => <FoodCard
+    //       key={item._id}
+    //       item={item}
+    //     ></FoodCard>)
+    //   }
+    // </div>
+    <>
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="grid md:grid-cols-3 gap-10">
+            {
+              items.slice(0, 6).map(item => <FoodCard
+                key={item._id}
+                item={item}
+              ></FoodCard>)
+            }
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className=''>
+          <div className="grid md:grid-cols-3 gap-10">
+            {
+              items.slice(7, 13).map(item => <FoodCard
+                key={item._id}
+                item={item}
+              ></FoodCard>)
+            }
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="grid md:grid-cols-3 gap-10">
+            {
+              items.slice(14, 20).map(item => <FoodCard
+                key={item._id}
+                item={item}
+              ></FoodCard>)
+            }
+          </div>
+        </SwiperSlide>
+
+      </Swiper>
+    </>
   );
 };
 
