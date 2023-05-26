3b35e91f371b2b7fc76d70868a4cc0b7cc9ccde7
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-26 13:02:48 +0600

    added signIn signOut function in auth provider

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { createContext } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 import { useEffect } from "react";
 
@@ -11,6 +11,21 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
+
+  const createUser = (email, password) => {
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  const signIn = (email, password) => {
+    setLoading(true)
+    return signInWithEmailAndPassword( email, password)
+  }
+
+  const logOut = () => {
+    setLoading(true)
+    return signOut(auth)
+  }
 
   useEffect( () => {
     const unsubscribe = onAuthStateChanged(auth, currentUser => {
@@ -27,6 +42,9 @@ const AuthProvider = ({ children }) => {
   const authInfo = {
     user,
     loading,
+    createUser,
+    signIn,
+    logOut,
 
   }
 
