0705ebb04d3c5af7dbdd6e476171f7c6503cc2c0
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-30 12:15:54 +0600

    create admin dashboard

--- src/Pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/Pages/Shared/SocialLogin/SocialLogin.jsx
@@ -26,11 +26,9 @@ const SocialLogin = () => {
           body: JSON.stringify(saveUser)
         })
           .then(res => res.json())
-          .then(data => {
-            if (data.insertedId) {
-              
+          .then(() => {
+              console.log('from', from)
               navigate(from, { replace: true })
-            }
           })
         
         
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./PrivateRoute";
 import Secret from "../Pages/Shared/Secret/Secret";
 import Dashboard from "../Layout/Dashboard";
 import MyCart from "../Pages/Dashboard/MyCart/MyCart";
+import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 
 export const router = createBrowserRouter([
   {
@@ -50,7 +51,11 @@ export const router = createBrowserRouter([
       {
         path: 'mycart',
         element: <MyCart></MyCart>
-      }
+      },
+      {
+        path: 'allusers',
+        element: <AllUsers></AllUsers>
+      },
     ]
   }
 ]);
