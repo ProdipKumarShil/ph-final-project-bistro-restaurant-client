2ace791469d715b713adfa307987071b25456e02
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-05-31 08:18:49 +0600

    added useAuth hook, modify command

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,12 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { FaTrashAlt, FaUser, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const { data: users = [], refetch } = useQuery(['users'], async () => {
     const res = await fetch('http://localhost:5000/users')
     return res.json()
   })
+
+  const handleMakeAdmin = user => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: 'PATCH'
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      if(data.modifiedCount){
+        refetch()
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: `${user.name} is an Admin Now`,
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    })
+  }
 
   const handleDelete = user => {
 
@@ -38,7 +59,7 @@ const AllUsers = () => {
                 <td>{user.email}</td>
                 <td>
                   {
-                    user.role === 'admin' ? 'admin' : <button onClick={() => handleDelete(user)} className="btn btn-ghost  bg-orange-600 text-white"><FaUserShield /></button>
+                    user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user)} className="btn btn-ghost  bg-orange-600 text-white"><FaUserShield /></button>
                   }
                 </td>
                 <td><button onClick={() => handleDelete(user)} className="btn btn-ghost  bg-red-600 text-white"><FaTrashAlt /></button></td>
